8a7f0aadc42956f0945ec9063283739cbd60c94d
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-24 22:05:39 +0300

    Refactor CSS files and add Tailwind CSS configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,21 @@ export default function App() {
     /**
      * ! STATE (état, données) de l'application
      */
-    
-    
+
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-    
-    
+
+
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
         <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </>
     )
 }
