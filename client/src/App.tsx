4f8be4d713e3baacdc7ae26b71685750c7ac058f
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-24 22:20:02 +0300

    Add Button component to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,6 @@
+import { Button } from "./components/ui/button"
+
+
 export default function App() {
     /**
      * ! STATE (état, données) de l'application
@@ -17,6 +20,7 @@ export default function App() {
             <h1 className="text-3xl font-bold underline">
                 Hello world!
             </h1>
+            <Button>Click me</Button>
         </>
     )
 }
