af0f5213129444e926a4b906b8e9c482a7c032c8
CousemaAnjary <anjaryabdillah@gmail.com>
2024-11-02 15:49:59 +0300

    Suppression de la couleur de fond du formulaire d'inscription pour une meilleure intégration visuelle

--- client/src/modules/auth/components/RegisterForm.tsx
+++ client/src/modules/auth/components/RegisterForm.tsx
@@ -17,7 +17,7 @@ export default function RegisterForm() {
     return (
         <>
             <div className="font-sans">
-                <div className="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
+                <div className="relative min-h-screen flex flex-col sm:justify-center items-center">
                     <div className="relative sm:max-w-sm w-full">
                         <div className="card bg-blue-400 shadow-lg w-full h-full rounded-3xl absolute transform -rotate-6"></div>
                         <div className="card bg-red-400 shadow-lg w-full h-full rounded-3xl absolute transform rotate-6"></div>
