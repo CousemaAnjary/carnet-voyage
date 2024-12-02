a411ac438b12a0a4c0f5b6b007bedb7871c73557
malcovys <malcovys22.aps@gmail.com>
2024-12-02 16:00:42 +0300

    Refactor CreateVoyageForm and DayController: remove console log, update transaction handling to return created day with photos, and enhance voyage slice with stateAddDay action for managing days in voyages

--- client/src/pages/app/days/components/DayUploader.tsx
+++ client/src/pages/app/days/components/DayUploader.tsx
@@ -6,15 +6,23 @@ import {
     DialogTitle, 
     DialogTrigger } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
-import { motion } from "framer-motion";
-import { PiCameraPlusBold } from "react-icons/pi";
-import { useState } from "react";
-import { postDay } from "../../../../features/api/services";
+import { motion } from "framer-motion"
+import { PiCameraPlusBold } from "react-icons/pi"
+import { useState } from "react"
+import { postDay } from "../../../../features/api/services"
+import { useAppDispatch } from "@/features/stores/hook"
+import { DayType } from "@/features/api/types"
+import { stateAddDay } from "@/features/stores/voyageSlice"
+import NetworkErrorDialog from "../../errors/NetworkErrorDialog"
 
 const DayUploader = ({voyageId}: {voyageId:number}) => {
-    const [images, setImages] = useState<Array<string>>([]);
-    const [legend, setLegend] = useState<string>("");
-    const [files, setFiles] = useState<File[]>([]); // Stockage des fichiers réels
+    const dispatch = useAppDispatch()
+
+    const [legend, setLegend] = useState<string>("")
+    const [images, setImages] = useState<Array<string>>([])
+    const [openNetworkErrorDialog, setOpenNetworkErrorDialog] = useState(false)
+
+    const [files, setFiles] = useState<File[]>([]) // Stockage des fichiers réels
 
     const handleUploadImages = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files
@@ -30,7 +38,7 @@ const DayUploader = ({voyageId}: {voyageId:number}) => {
 
                 // Ajouter le fichier à la liste
                 newFiles.push(file)
-            });
+            })
 
             // Mettre à jour l'état
             setImages((prev) => [...prev, ...newPreviews])
@@ -49,9 +57,14 @@ const DayUploader = ({voyageId}: {voyageId:number}) => {
         files.forEach((file) => formData.append("img[]", file))
 
         await postDay(formData)
+            .then(response => {
+                console.log(response)
+                const newDay : DayType = response.day
+                dispatch(stateAddDay({ voyage_id: voyageId, day: newDay }))
+            })
             .catch(error => {
                 if(error.message === "Network Error") {
-                    console.log("Erreur réseau lors de l'ajout de la journée.")
+                    setOpenNetworkErrorDialog(true)
                 } else {
                     console.log(error)
                 }
@@ -59,65 +72,71 @@ const DayUploader = ({voyageId}: {voyageId:number}) => {
     }
 
     return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button className="bg-blue-600 hover:bg-blue-500" >Nouvelle Journée 🔖</Button>
-            </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle className="text-lg md:text-2xl text-neutral-600 font-bold text-center mb-4">
-                        Encore une belle journée à ajouter 🌞
-                    </DialogTitle>
-                </DialogHeader>
-                <form onSubmit={handleSubmit} encType="multipart/form-data" className="flex-col space-y-5">
-                    <div className="flex justify-center items-center w-full h-[8rem] md:h-48 overflow-y-hidden overflow-x-auto">
-                        {images.map((image, idx) => (
-                            <motion.div key={"images" + idx}
-                                style={{ rotate: Math.random() * 20 - 10, }}
-                                whileHover={{ scale: 1.1, rotate: 0, zIndex: 100, }}
-                                whileTap={{ scale: 1.1, rotate: 0, zIndex: 100, }}
-                                className="rounded-xl -mx-4 p-1 bg-white border border-neutral-100 flex-shrink-0"
-                            >
-                                <img src={image} alt="day-image" width="500" height="500"
-                                    className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0"
-                                />
-                            </motion.div>
-                        ))}
+        <>
+            {openNetworkErrorDialog ? (
+                <NetworkErrorDialog setIsOpen={setOpenNetworkErrorDialog} />
+            ):(
+                <Dialog>
+                        <DialogTrigger asChild>
+                            <Button className="bg-blue-600 hover:bg-blue-500" >Nouvelle Journée 🔖</Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle className="text-lg md:text-2xl text-neutral-600 font-bold text-center mb-4">
+                                    Encore une belle journée à ajouter 🌞
+                                </DialogTitle>
+                            </DialogHeader>
+                            <form onSubmit={handleSubmit} encType="multipart/form-data" className="flex-col space-y-5">
+                                <div className="flex justify-center items-center w-full h-[8rem] md:h-48 overflow-y-hidden overflow-x-auto">
+                                    {images.map((image, idx) => (
+                                        <motion.div key={"images" + idx}
+                                            style={{ rotate: Math.random() * 20 - 10, }}
+                                            whileHover={{ scale: 1.1, rotate: 0, zIndex: 100, }}
+                                            whileTap={{ scale: 1.1, rotate: 0, zIndex: 100, }}
+                                            className="rounded-xl -mx-4 p-1 bg-white border border-neutral-100 flex-shrink-0"
+                                        >
+                                            <img src={image} alt="day-image" width="500" height="500"
+                                                className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0"
+                                            />
+                                        </motion.div>
+                                    ))}
 
-                        {/* uploader */}
-                        <motion.div key="uploader"
-                            style={{ rotate: Math.random() * 20 - 10, }}
-                            whileHover={{ scale: 1.1, rotate: 0, zIndex: 100, }}
-                            whileTap={{ scale: 1.1, rotate: 0, zIndex: 100, }}
-                            className="rounded-xl -mx-4 p-1 bg-white border border-neutral-100 flex-shrink-0"
-                        >
-                            <div className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0">
-                                <PiCameraPlusBold className="w-full h-full bg-slate-100 rounded-lg"/>
-                                <input className="cursor-pointer absolute inset-0 w-full h-full opacity-0"
-                                    type="file"
-                                    name="files[]"
-                                    multiple={true}
-                                    accept="image/png, image/jpg, image/jpeg"
-                                    onChange={handleUploadImages}
-                                />
-                            </div>
-                        </motion.div>
-                    </div>
-                    <div className="flex justify-center items-center">
-                        <textarea className="w-full border-none h-36"
-                            placeholder="Comment ça été ? ..." 
-                            value={legend}
-                            aria-description="Un journée génial ? 🙃"
-                            onChange={(e) => setLegend(e.target.value)}
-                            name="day-legend" id="day-legend" maxLength={255}
-                        />
-                    </div>
-                    <DialogFooter>
-                        <Button className="mt-5 w-full bg-blue-700 hover:bg-blue-500 text-md h-10" type="submit">Ajouter la journée</Button>
-                    </DialogFooter>
-                </form>
-            </DialogContent>
-    </Dialog>
+                                    {/* uploader */}
+                                    <motion.div key="uploader"
+                                        style={{ rotate: Math.random() * 20 - 10, }}
+                                        whileHover={{ scale: 1.1, rotate: 0, zIndex: 100, }}
+                                        whileTap={{ scale: 1.1, rotate: 0, zIndex: 100, }}
+                                        className="rounded-xl -mx-4 p-1 bg-white border border-neutral-100 flex-shrink-0"
+                                    >
+                                        <div className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0">
+                                            <PiCameraPlusBold className="w-full h-full bg-slate-100 rounded-lg"/>
+                                            <input className="cursor-pointer absolute inset-0 w-full h-full opacity-0"
+                                                type="file"
+                                                name="files[]"
+                                                multiple={true}
+                                                accept="image/png, image/jpg, image/jpeg"
+                                                onChange={handleUploadImages}
+                                            />
+                                        </div>
+                                    </motion.div>
+                                </div>
+                                <div className="flex justify-center items-center">
+                                    <textarea className="w-full border-none h-36"
+                                        placeholder="Comment ça été ? ..." 
+                                        value={legend}
+                                        aria-description="Un journée génial ? 🙃"
+                                        onChange={(e) => setLegend(e.target.value)}
+                                        name="day-legend" id="day-legend" maxLength={255}
+                                    />
+                                </div>
+                                <DialogFooter>
+                                    <Button className="mt-5 w-full bg-blue-700 hover:bg-blue-500 text-md h-10" type="submit">Ajouter la journée</Button>
+                                </DialogFooter>
+                            </form>
+                        </DialogContent>
+                </Dialog>
+            )}
+        </>
     )
 }
 
