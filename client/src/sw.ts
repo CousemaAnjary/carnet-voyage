b94648baaa32846880d73ae1c8a886f6cb3cce15
malcovys <malcovys22.aps@gmail.com>
2024-12-05 12:59:10 +0300

    Refactor service worker and Vite configuration: enhance caching logic, add install and activate event listeners, and update PWA settings for improved asset management

--- client/src/sw.ts
+++ client/src/sw.ts
@@ -3,26 +3,35 @@ import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from
 import { clientsClaim } from 'workbox-core'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
 
-declare let self: ServiceWorkerGlobalScope
+declare let self: ServiceWorkerGlobalScope;
 
 // self.__WB_MANIFEST is the default injection point
-precacheAndRoute(self.__WB_MANIFEST)
+precacheAndRoute(self.__WB_MANIFEST);
 
-// clean old assets
-cleanupOutdatedCaches()
+// clean old Workbox cached assets
+cleanupOutdatedCaches();
 
-let allowlist: RegExp[] | undefined
+let allowlist: RegExp[] | undefined;
 // in dev mode, we disable precaching to avoid caching issues
 if (import.meta.env.DEV)
-  allowlist = [/^\/$/]
+  allowlist = [/^\/$/];
 
 // to allow work offline
 registerRoute(new NavigationRoute(
   createHandlerBoundToURL('index.html'),
   { allowlist },
-))
+));
 
-self.skipWaiting()
-clientsClaim()
+self.addEventListener('install', () => {
+  console.log('Service worker installed')
+  self.skipWaiting() // Activate worker immediately
+});
+self.addEventListener('activate', () => {
+  console.log('Service worker activated')
+  clientsClaim() // Become available to all pages
+});
+
+
+
 
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,10 +6,10 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    registerType: 'prompt', // Permet d'afficher un popup si le sw à été mis à jour
     strategies: 'injectManifest',
     
     injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
       swSrc: path.resolve(__dirname, 'src/sw.ts'),
       swDest: 'sw.js',
     },
@@ -19,24 +19,6 @@ export default defineConfig({
       short_name: 'carnet-voyage',
       description: 'carnet-voyage',
       theme_color: '#ffffff',
-    },
-
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      runtimeCaching: [{
-        urlPattern: new RegExp(`^${process.env.API_URL}/?.*`),
-        handler: "NetworkFirst",
-        options: {
-          cacheName: 'api-responses-caches',
-          expiration: {
-            maxEntries: 1,
-            maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }]
     },
 
     devOptions: {
