b1ab8fc3f83fb20671703126f98b468f1d072ea5
malcovys <malcovys22.aps@gmail.com>
2024-12-08 23:53:30 +0300

    Enhance Photo component navigation: add next and previous photo functionality with buttons, improve exit navigation, and refactor photo selection logic

--- client/src/pages/app/days/Photo.tsx
+++ client/src/pages/app/days/Photo.tsx
@@ -1,6 +1,6 @@
 import Error404 from "@/components/errors/Error404";
 import { useAppSelector } from "@/features/stores/hook";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { RxCross2 } from "react-icons/rx";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
@@ -10,6 +10,8 @@ const useQuery = () => {
 
 const Photo  = () => {
     const query = useQuery();
+    const navigate = useNavigate();
+    const pathname = useLocation().pathname
     const { voyages } = useAppSelector((state) => state.voyages);
 
     const vid = query.get('vid');
@@ -20,7 +22,31 @@ const Photo  = () => {
                     ?.days?.find(day => day.id === parseInt(did || '-1', 10))
                     ?.day_photos;
 
-    const photo = photos?.find(photo => photo.id === parseInt(pid || '-1', 10));
+    const selectPhoto = (id:number) => {
+        return photos?.find(photo => photo.id === id);
+    };
+
+    const photo = pid ? selectPhoto(parseInt(pid, 10)) : undefined;
+    const nextPhoto = photo ? selectPhoto(photo.id - 1) : undefined;
+    const prevPhoto = photo ? selectPhoto(photo.id + 1) : undefined;
+
+    const navigateToPhoto = (photoId:number) => {
+        navigate(`${pathname}?vid=${vid}&did=${did}&pid=${photoId}`)
+    };
+    
+    const next = () => {
+        if(!nextPhoto) return
+        navigateToPhoto(nextPhoto.id)
+    };
+
+    const prev = () => {
+        if(!prevPhoto) return
+        navigateToPhoto(prevPhoto.id)
+    };
+
+    const exit = () => {
+        navigate(`/gallery/${photo?.day_id}`)
+    }
     
     return (
         <>
@@ -31,24 +57,28 @@ const Photo  = () => {
                     <button
                         className="fixed top-0 right-2 text-white rounded-full p-4"
                         aria-label="Créer un nouveau dossier"
-                        onClick={() => console.log('quitte')}
+                        onClick={exit}
                     ><RxCross2 size={30}/></button>
                     <div className="flex items-center justify-center">
-                        <button
-                            className="fixed right-1 bg-white rounded-full p-3 opacity-50 hover:opacity-100"
-                            aria-label="Créer un nouveau dossier"
-                            onClick={() => console.log('next')}
-                        ><IoIosArrowForward size={30}/></button>
+                        {prevPhoto && (
+                            <button
+                                className="fixed left-1 bg-white rounded-full p-3 opacity-50 hover:opacity-100"
+                                aria-label="Créer un nouveau dossier"
+                                onClick={prev}
+                            ><IoIosArrowBack size={30}/></button>
+                        )}
                         <img
                             className="max-w-[90%]"
                             src={`${import.meta.env.VITE_BACKEND_API_URL}${photo.photo_url}`}
                             alt={`photo-${photo.id}`}
                         />
-                        <button
-                            className="fixed left-1 bg-white rounded-full p-3 opacity-50 hover:opacity-100"
-                            aria-label="Créer un nouveau dossier"
-                            onClick={() => console.log('prev')}
-                        ><IoIosArrowBack size={30}/></button>
+                        {nextPhoto && (
+                            <button
+                                className="fixed right-1 bg-white rounded-full p-3 opacity-50 hover:opacity-100"
+                                aria-label="Créer un nouveau dossier"
+                                onClick={next}
+                            ><IoIosArrowForward size={30}/></button>
+                        )}
                     </div>
                 </div>
             )}
