6812bd36f6974f99d9b969b87a26be3fc6298845
malcovys <malcovys22.aps@gmail.com>
2024-12-12 16:53:46 +0300

    Update API URL in environment config and refactor service worker caching logic: streamline API endpoint and enhance photo caching mechanism

--- client/src/features/api/apiConfig.ts
+++ client/src/features/api/apiConfig.ts
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_BACKEND_API_URL + '/api'
+const API_URL = import.meta.env.VITE_BACKEND_API_URL;
 
 // Configuration de l'instance axios
 const api = axios.create({
